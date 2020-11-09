a6f08c622acc5926c3cf080b04337ffa93c5af81
Tushar Ranjan Sahoo <tushar2289@gmail.com>
2020-11-09 11:39:03 +0530

    first commit

---
+++ packages/product-one/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { ExampleComponent} from 'ui-components';
+import 'ui-components/dist/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ExampleComponent text="Create React Library Example 😄" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui-components/dist/index.modern.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var styles = {"test":"_styles-module__test__3ybTi","hello":"_styles-module__hello__1IijS"};
+
+const ExampleComponent = ({
+  text
+}) => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test
+  }, "Example Component: ", text);
+};
+
+export { ExampleComponent };
+//# sourceMappingURL=index.modern.js.map
